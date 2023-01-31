7f8c775d72a03eb47794fc0c7a5bf9552c8ca0e5
EngAliSaleh <al3qrabxxo@gmail.com>
2023-01-31 09:32:22 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+document.oncontextmenu = function(){return false;}
+
 let sections = document.querySelectorAll('section');
 let navA = document.querySelectorAll('nav .contanier ul li a');
 let arrow = document.querySelector('.arrow');
@@ -5,82 +7,82 @@ let Bar = document.querySelector('.bar')
 let Links = document.querySelector('nav ul')
 
 
-Bar.onclick = function(){
+Bar.onclick = function () {
     Links.classList.toggle('show-ul');
 }
 
 
 
 
-window.onscroll = function(){
+window.onscroll = function () {
     let current = '';
-    
+
     // console.log(scrollY);
 
     sections.forEach(section => {
-    let sectionTop = section.offsetTop - 300;
+        let sectionTop = section.offsetTop - 300;
         // console.log(sectionTop);
-        
-        if(scrollY >= sectionTop) {
-            current = section.getAttribute('id') ;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute('id');
         }
 
-    })   
+    })
     // console.log(current);
 
 
 
 
-    navA.forEach( a => {
+    navA.forEach(a => {
         a.classList.remove('active');
-        if(a.classList.contains(current)){
-             a.classList.add('active')
+        if (a.classList.contains(current)) {
+            a.classList.add('active')
         }
-        
+
     });
 }
 
 window.onscroll = function () {
     if (scrollY > 300) {
- 
-       arrow.style.opacity = 1
- 
+
+        arrow.style.opacity = 1
+
     }
     else {
-       arrow.style.opacity = 0
+        arrow.style.opacity = 0
     }
- }
- 
- arrow.onclick = function () {
+}
+
+arrow.onclick = function () {
     window.scrollTo({
-       top: 0,
-       left: 0,
-       behavior: "smooth"
+        top: 0,
+        left: 0,
+        behavior: "smooth"
     })
- }
+}
 
 
 
 
-window.onscroll = function(){
+window.onscroll = function () {
     let current = '';
-    
+
     // console.log(scrollY);
 
     sections.forEach(section => {
-    let sectionTop = section.offsetTop;
+        let sectionTop = section.offsetTop;
         // console.log(sectionTop);
-        if(scrollY >= sectionTop - 500) {
+        if (scrollY >= sectionTop - 500) {
             // current = section.getAttribute('id') 
-            navA.forEach( a => {
+            navA.forEach(a => {
                 a.classList.remove('active');
-                if(a.classList.contains(section.getAttribute('id'))){
-                     a.classList.add('active')
+                if (a.classList.contains(section.getAttribute('id'))) {
+                    a.classList.add('active')
                 }
-                
+
             });
         }
-    })   
+    })
     // console.log(current);
-    
+
 }
